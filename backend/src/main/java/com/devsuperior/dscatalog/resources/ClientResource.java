4186655a78acbe1452198be92a25af5ce7b10df7
Danilo George <danilo.george@hotmail.com>
2021-06-20 18:32:52 -0300

    refatoração pageable

--- backend/src/main/java/com/devsuperior/dscatalog/resources/ClientResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/ClientResource.java
@@ -1,11 +1,11 @@
 package com.devsuperior.dscatalog.resources;
 
+
 import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -28,17 +27,19 @@ public class ClientResource {
 	@Autowired
 	private ClientService service;
 	
+	/* 
+	 * Simplificação do método trocar o PageRequest pelo Pageable mesmos
+	 * parametros de entrada
+	 * 
+	 	@RequestParam(value = "page", defaultValue = "0") Integer page,
+		@RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
+		@RequestParam(value = "direction", defaultValue = "ASC") String direction,
+		@RequestParam(value = "orderBy", defaultValue = "name") String orderBy
+	 *
+	 */
 	@GetMapping
-	public ResponseEntity<Page<ClientDTO>> findAll(
-			@RequestParam(value = "page", defaultValue = "0") Integer page,
-			@RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
-			@RequestParam(value = "direction", defaultValue = "ASC") String direction,
-			@RequestParam(value = "orderBy", defaultValue = "name") String orderBy
-			) {
-		
-		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
-				
-		Page<ClientDTO> list = service.findAllPaged(pageRequest);
+	public ResponseEntity<Page<ClientDTO>> findAll(Pageable pageable) {				
+		Page<ClientDTO> list = service.findAllPaged(pageable);
 		
 		return ResponseEntity.ok().body(list);
 	}
